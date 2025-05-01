84dd8086c172b065aa3e91eac43836ff46db866c
Tonu Cristian <chrisdev2002@gmail.com>
2025-05-01 21:45:03 +0300

    feat: added responsiveness

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,7 +3,7 @@ import LoginForm from "../components/authentication/components/login/LoginForm";
 export default function LoginPage() {
   return (
     <div className="flex h-screen items-center justify-center">
-      <div className="border-primary w-20p rounded-lg border-2 p-4">
+      <div className="border-primary xxs:w-4/5 xs:w-80 w-11/12 rounded-lg border-2 p-4">
         <h1 className="mb-4 text-center text-2xl font-semibold">Login</h1>
         <LoginForm />
       </div>
