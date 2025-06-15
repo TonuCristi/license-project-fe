61ac86c088efb28e8283a3ef5e381b8882a64a9a
Tonu Cristi <chrisdev2002@gmail.com>
2025-06-15 03:25:23 +0300

    feat: added state and progress to project model and modified fetch based on them

--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -8,7 +8,7 @@ export default function ProjectPage() {
 
   return (
     <main className="border-primary scrollbar xs:gap-5 m-auto flex h-full w-full flex-col gap-2 overflow-y-auto border-x-2 p-2 sm:p-4 lg:w-5xl">
-      <PageTitle>Projects</PageTitle>
+      <PageTitle>Project</PageTitle>
     </main>
   );
 }
